d4ec39d330f1a8eacadb4e27f1506ff926aa63e7
Vincent Waller <vrwaller@gmail.com>
2026-06-28 10:10:56 -0700

    Update kitout to 2.1.0

--- Formula/kitout.rb
+++ Formula/kitout.rb
@@ -1,18 +1,18 @@
 class Kitout < Formula
   desc "Declarative setup tool for fresh Macs"
   homepage "https://github.com/vwall/kitout"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/vwall/kitout/releases/download/v2.0.0/kitout_2.0.0_darwin_arm64.tar.gz"
-      sha256 "94f1c41f3d634cbad351d19dddb978efa370dee68bf03ac97c108caaaf932b91"
+      url "https://github.com/vwall/kitout/releases/download/v2.1.0/kitout_2.1.0_darwin_arm64.tar.gz"
+      sha256 "0b348f5919b8ffd4ce19672885e788572f21aacc82c742d10ae4248aeeca319f"
     end
 
     on_intel do
-      url "https://github.com/vwall/kitout/releases/download/v2.0.0/kitout_2.0.0_darwin_amd64.tar.gz"
-      sha256 "3bf66fdcf88d7a79203a0bf4b6c13b7681cf42c775df3e0677f3357fc9d7057c"
+      url "https://github.com/vwall/kitout/releases/download/v2.1.0/kitout_2.1.0_darwin_amd64.tar.gz"
+      sha256 "c0d74f2db0dd26bd7536837afb693ed529f053405c577b56e3259320b6be6869"
     end
   end
 
